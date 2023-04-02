1d0f8381e8cbcb60a07ddaddaefc260424b12e9f
yu07na06 <87966321+yu07na06@users.noreply.github.com>
2023-04-02 21:48:39 +0900

    [Silver III] Title: 이친수, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+            long[][] D = new long[N+1][2];
+            D[1][1]=1;
+            D[1][0]=0;
+
+            for(int i=2; i<=N; i++){
+                D[i][0] = D[i-1][0] + D[i-1][1];
+                D[i][1] = D[i-1][0];
+            }
+
+            System.out.println(D[N][0]+D[N][1]);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
